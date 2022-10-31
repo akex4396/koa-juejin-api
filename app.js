7f5037fdb03ebf7417335bf7c975cde130f347a6
alex <2844520415@qq.com>
2022-10-31 13:04:17 +0800

    feat: 日志打印与日志写入

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+const fs = require('fs')
 const Koa = require('koa')
 const cors = require('koa2-cors')
 const helmet = require('koa-helmet')
 const bodyParser = require('koa-bodyparser')
-const fs = require('fs')
+const morgan = require('koa-morgan')
 
+const logger = require('koa-logger')
 const Router = require('koa-router')
 const path = require('path')
 const router = new Router()
 const app = new Koa()
+
+app.use(logger())
+
+const accessLogStream = fs.createWriteStream(__dirname + '/access.log')
+app.use(morgan('short', { stream: accessLogStream }))
 
 // 防止html注入攻击
 app.use(helmet())
@@ -39,10 +46,10 @@ function defineRouter(routesPath) {
     } else {
       // 加载路由模块
       let route = require(elementPath)
-      // 获取v2前缀 [ 'X:\\item\\koa-juejin-api\\routes', 'v2' ]
 
       const prefixArr = routesPath.split('\\')
       let routePrefix = `/${prefixArr[prefixArr.length - 1]}` || '/v1'
+
       //api路径: 127.0.0.1:3000/v2/users
       router.use(routePrefix + '/' + element.replace('.js', ''), route.routes())
     }
